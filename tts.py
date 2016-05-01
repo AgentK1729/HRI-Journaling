325e9350e71b42a0597e0f796c11ee015f40a185
Tejas Sathe <sathe.tejas@gmail.com>
2016-05-01 13:01:30 -0400

    Ported to python 3

--- tts.py
+++ tts.py
@@ -1,11 +1,4 @@
-import pyttsx
-
-def speak():
-	engine = pyttsx.init()
-	engine.say("hello!")
-	engine.say("how")
-	engine.say("are")
-	engine.say("you")
-	engine.say("feeling")
-	engine.say("today?")
-	engine.runAndWait()	
+from gtts import gTTS
+greeting = ("Please make a journal entry")
+tts = gTTS(text=greeting, lang='en')
+tts.save("alert.mp3")
